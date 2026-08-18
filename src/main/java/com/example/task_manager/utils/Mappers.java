3bc41950d7ec9e61fea5d62538eaaade801d3a0c
Sergio Castellano Betancor <sergiocastellanotrabajo@gmail.com>
2026-08-18 21:21:08 +0200

    :sparkles: ModelMapper

---
+++ src/main/java/com/example/task_manager/utils/Mappers.java
@@ -0,0 +1,27 @@
+package com.example.task_manager.utils;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class Mappers {
+
+    @Autowired
+    ModelMapper mapper;
+
+    public <S, T> List<T> mapList(List<S> source, Class<T> targetClass) {
+        return source
+                .stream()
+                .map(element -> mapper.map(element, targetClass))
+                .toList();
+    }
+
+    public <S, T> T map(S source, Class<T> targetClass) {
+        return mapper.map(source, targetClass);
+    }
+}
+
+
